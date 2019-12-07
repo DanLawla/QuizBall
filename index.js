a483e6344498fe932e4415be613467436faf05cb
DanLawla <32523954+DanLawla@users.noreply.github.com>
2019-12-06 19:27:29 -0500

    Javascript for login

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 //var firebaseRef 
-const firebaseConfig = {
-  apiKey: "AIzaSyAYnWDG8_q1XGELrjxhWznEItDVsU8Knq0",
-  authDomain: "quiz-ball.firebaseapp.com",
-  databaseURL: "https://quiz-ball.firebaseio.com",
-  projectId: "quiz-ball",
-  storageBucket: "quiz-ball.appspot.com",
-  messagingSenderId: "574301705322",
-  appId: "1:574301705322:web:89936cca660370ffe02613",
-  measurementId: "G-SXPFWLY417"
-};
-firebase.initializeApp(firebaseConfig);
+// const firebaseConfig = {
+//   apiKey: "AIzaSyAYnWDG8_q1XGELrjxhWznEItDVsU8Knq0",
+//   authDomain: "quiz-ball.firebaseapp.com",
+//   databaseURL: "https://quiz-ball.firebaseio.com",
+//   projectId: "quiz-ball",
+//   storageBucket: "quiz-ball.appspot.com",
+//   messagingSenderId: "574301705322",
+//   appId: "1:574301705322:web:89936cca660370ffe02613",
+//   measurementId: "G-SXPFWLY417"
+// };
+// firebase.initializeApp(firebaseConfig);
 
 // signup
 const signupForm = document.querySelector('#signup-form');
@@ -19,16 +19,17 @@ signupForm.addEventListener('submit', (e) => {
   // get user info
   const email = signupForm['signup-email'].value;
   const password = signupForm['signup-password'].value;
-  const db = firebase.database().ref();
+  
 
   // sign up the user
   firebase.auth().createUserWithEmailAndPassword(email, password).then(cred => {
-    console.log(cred.user);
-    var user = firebase.auth().currentUser;
-    var uid = user.uid;
-    db.child(''+ uid ).set(0);
-    window.alert("user id is " + uid);
+    return firebase.db.collection('users').doc(cred.user.uid).set({
+      score: 0
+    });
+  }).then(() => {
     window.location ="main.html";
+  
+    
   }).catch(function(error) {
     // Handle Errors here.
     var errorCode = error.code;
@@ -38,13 +39,13 @@ signupForm.addEventListener('submit', (e) => {
   });
 });
 
-firebase.auth().onAuthStateChanged(function(user) {
-  if (user) {
-    // User is signed in.
-  } else {
-    // No user is signed in.
-  }
-});
+// auth().onAuthStateChanged(function(user) {
+//   if (user) {
+//     // User is signed in.
+//   } else {
+//     // No user is signed in.
+//   }
+// });
 
 function login(){
 
